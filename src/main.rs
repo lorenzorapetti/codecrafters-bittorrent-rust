ee510a45d537a3099c05f6f6a6329b9fd3b4e458
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2026-06-09 15:46:07 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,42 @@
 use std::env;
 
-use serde_bencode;
+fn decode_bencoded_value(encoded_value: &str) -> anyhow::Result<serde_json::Value> {
+    let value: serde_bencode::value::Value = serde_bencode::from_str(encoded_value)
+        .unwrap_or_else(|_| panic!("Failed to decode bencoded value: {}", encoded_value));
 
-#[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    serde_bencode::from_str(encoded_value)
-        .unwrap_or_else(|_| panic!("Failed to decode bencoded value: {}", encoded_value))
-    // // If encoded_value starts with a digit, it's a number
-    // if encoded_value.chars().next().unwrap().is_ascii_digit() {
-    //     // Example: "5:hello" -> "hello"
-    //     let colon_index = encoded_value.find(':').unwrap();
-    //     let number_string = &encoded_value[..colon_index];
-    //     let number = number_string.parse::<usize>().unwrap();
-    //     let string = &encoded_value[colon_index + 1..colon_index + 1 + number];
-    //     serde_json::Value::String(string.to_string())
-    // } else {
-    //     panic!("Unhandled encoded value: {}", encoded_value)
-    // }
+    decode(value)
+}
+
+fn decode(value: serde_bencode::value::Value) -> anyhow::Result<serde_json::Value> {
+    match value {
+        serde_bencode::value::Value::Bytes(b) => {
+            let string = String::from_utf8(b)?;
+            Ok(serde_json::Value::String(string))
+        }
+        serde_bencode::value::Value::Int(i) => Ok(serde_json::Value::Number(i.into())),
+        serde_bencode::value::Value::List(l) => Ok(serde_json::Value::Array(
+            l.into_iter()
+                .map(|item| decode(item))
+                .collect::<anyhow::Result<Vec<serde_json::Value>>>()?,
+        )),
+        _ => {
+            panic!("Unhandled encoded value: {:?}", value)
+        }
+    }
 }
 
 // Usage: your_program.sh decode "<encoded_value>"
-fn main() {
+fn main() -> anyhow::Result<()> {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
 
     if command == "decode" {
         let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value);
+        let decoded_value = decode_bencoded_value(encoded_value)?;
         println!("{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1])
     }
+
+    Ok(())
 }
